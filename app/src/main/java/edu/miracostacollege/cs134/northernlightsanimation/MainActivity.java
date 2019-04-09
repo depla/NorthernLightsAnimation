860aecd213d0b065fe247cb23fd795cfba6abd61
4803AW16 <4803AW16@usa.miracosta.cc.ca.us>
2019-04-09 15:15:27 -0700

    Initial commit

---
+++ app/src/main/java/edu/miracostacollege/cs134/northernlightsanimation/MainActivity.java
@@ -0,0 +1,65 @@
+package edu.miracostacollege.cs134.northernlightsanimation;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView lightsImageView;
+
+    //animationdrawable used for frame animations
+    private AnimationDrawable frameAnim;
+
+    //animation used for tweened animations
+    private Animation rotateAnim;
+    private Animation shakeAnim;
+    private Animation customAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lightsImageView = findViewById(R.id.lightsImageView);
+    }
+
+    //method for the frame animation button
+    public void toggleFrameAnim(View v)
+    {
+        if(frameAnim == null)
+        {
+            lightsImageView.setBackgroundResource(R.drawable.frame_anim);
+
+            frameAnim = (AnimationDrawable) lightsImageView.getBackground();
+        }
+
+        //if the animation is running stop it
+        if(frameAnim.isRunning())
+        {
+            frameAnim.stop();
+        }
+        else //start it
+        {
+            frameAnim.start();
+        }
+    }
+
+    public void toggleRotateAnim(View v)
+    {
+        rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+
+        lightsImageView.startAnimation(rotateAnim);
+    }
+
+    public void toggleShakeAnim(View v)
+    {
+        shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_anim);
+
+        lightsImageView.startAnimation(shakeAnim);
+    }
+}
